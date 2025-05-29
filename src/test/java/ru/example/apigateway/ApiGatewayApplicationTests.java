577cef7883a9b1f7ee312c0dd78d2b63b429d213
Yuri Luttsev <floomyz@vk.com>
2025-05-29 22:37:19 +0300

    Добавлены дефолтные значения переменных для теста контекста

--- src/test/java/ru/example/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/ru/example/apigateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,9 @@ package ru.example.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"security.access.secret=test"
+})
 class ApiGatewayApplicationTests {
 
 	@Test
