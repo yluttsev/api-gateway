930aff8b3c52ab5393997841b449caab799b7780
Yuri Luttsev <floomyz@vk.com>
2025-06-04 16:06:51 +0300

    Добавлен эндпоинт для websocket

--- src/main/java/ru/example/apigateway/filter/JwtValidatorGatewayFilterFactory.java
+++ src/main/java/ru/example/apigateway/filter/JwtValidatorGatewayFilterFactory.java
@@ -1,22 +1,71 @@
 package ru.example.apigateway.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import ru.example.apigateway.payload.MessageResponse;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtValidatorGatewayFilterFactory extends AbstractGatewayFilterFactory<JwtValidatorGatewayFilterFactory.Config> {
 
-    private final JwtFilter jwtFilter;
+    private final ObjectMapper objectMapper;
 
-    public JwtValidatorGatewayFilterFactory(JwtFilter jwtFilter) {
+    @Value("${security.access.secret}")
+    private String accessSecret;
+
+    public JwtValidatorGatewayFilterFactory(ObjectMapper objectMapper) {
         super(Config.class);
-        this.jwtFilter = jwtFilter;
+        this.objectMapper = objectMapper;
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        return jwtFilter;
+        return (exchange, chain) -> {
+            String token = exchange.getRequest().getQueryParams().getFirst("token");
+            if (token == null || token.isEmpty()) {
+                String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    token = authHeader.substring(7);
+                }
+            }
+
+            if (token != null && token.startsWith("Bearer ")) {
+                try {
+                    JWT.require(Algorithm.HMAC256(accessSecret)).build().verify(token);
+                    return chain.filter(exchange);
+                } catch (TokenExpiredException e) {
+                    return buildResponse(HttpStatus.UNAUTHORIZED, "Access токен просрочен", exchange);
+                } catch (Exception e) {
+                    return buildResponse(HttpStatus.UNAUTHORIZED, "Неверный access токен", exchange);
+                }
+            } else {
+                return buildResponse(HttpStatus.UNAUTHORIZED, "Неверный формат access токена", exchange);
+            }
+        };
+    }
+
+    @SneakyThrows
+    private Mono<Void> buildResponse(HttpStatus status, String message, ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(status);
+        exchange.getResponse().getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString());
+        DataBuffer buffer = exchange.getResponse()
+                .bufferFactory()
+                .wrap(objectMapper.writeValueAsString(new MessageResponse(message)).getBytes(StandardCharsets.UTF_8));
+        return exchange.getResponse().writeWith(Mono.just(buffer));
     }
 
     public static class Config {
